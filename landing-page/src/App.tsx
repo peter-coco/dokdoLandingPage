3e14e6a34c9762e29218ffef5c0a786932174690
peter <dklirser@gmail.com>
2021-04-23 22:26:21 +0900

    main: HTML & CSS, Container-notice: HTML & CSS

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
+
 import "./App.css";
+// import video from "./images/videoSample.mp4";
+import { stringify } from "node:querystring";
 
 function Header() {
   return (
@@ -81,16 +84,110 @@ function SideBar() {
 }
 
 function Main() {
-  return;
+  const [videoTitle, setVideoTitle] = useState<string>(
+    "./images/videoSample.mp4"
+  );
+  return (
+    <div id="main-wrap">
+      <div id="main-video">
+        <video
+          id="main-video-src"
+          width="auto"
+          autoPlay={true}
+          muted={true}
+          loop={true}
+        >
+          <source
+            // src="./images/videoSample.mp4"
+            src={videoTitle}
+            type="video/mp4"
+            id="video-src"
+          />
+        </video>
+        <div id="video-OnOffBtn">
+          <div id="video-onOffBtnImg"></div>
+          <div id="video-onOffBtnText">STOP</div>
+        </div>
+      </div>
+      <div id="main-link">
+        <div id="main-link-openCloseBtn">
+          <div id="openCloseTop"></div>
+          <div id="openCloseBottom"></div>
+          <div id="openCloseText">OPEN</div>
+        </div>
+      </div>
+      <div id="main-picture-dokdo">
+        <div id="main-picture-cloud"></div>
+      </div>
+    </div>
+  );
+}
+
+function Container() {
+  return (
+    <div id="container-wrap">
+      <div id="container-notice">
+        <div id="notice-head">
+          <div id="notice-head-introduce">
+            <div id="notice-head-English-bar"></div>
+            <div id="notice-head-English-Text">Notice</div>
+            <div id="notice-head-Title">재단소식</div>
+            <div id="notice-haed-Subscrition">
+              독도재단의 이슈와
+              <br />
+              정보를 안내해 드립니다.
+            </div>
+          </div>
+          <div id="notice-head-contents-title">
+            <div className="head-content-title head-content-title-active">
+              공지사항
+            </div>
+            <div className="head-content-title">언론 & 홍보</div>
+            <div className="head-content-title">보도자료</div>
+          </div>
+          <div id="notice-head-contents-moreBtn">
+            <div id="moreBtn-horizontal"></div>
+            <div id="moreBtn-vertical"></div>
+          </div>
+        </div>
+        <div id="notice-contents">
+          <div className="content active">
+            <div className="content-title"></div>
+            <div className="content-subscrition"></div>
+            <div className="content-date"></div>
+          </div>
+          <div className="content">
+            <div className="content-title"></div>
+            <div className="content-subscrition"></div>
+            <div className="content-date"></div>
+          </div>
+          <div className="content">
+            <div className="content-title"></div>
+            <div className="content-subscrition"></div>
+            <div className="content-date"></div>
+          </div>
+        </div>
+      </div>
+      <div id="container-eventSchedule"></div>
+      <div id="container-magazine"></div>
+      <div id="contaienr-logo"></div>
+    </div>
+  );
+}
+
+function Footer() {
+  return <div id="footer-wrap"></div>;
 }
 
 function App() {
   return (
     <>
-      <div id="main-wr">
+      <div id="app-wrap">
         <SideBar />
         <Header />
-        {/* <Main /> */}
+        <Main />
+        <Container />
+        <Footer />
       </div>
     </>
   );
